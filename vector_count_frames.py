f1d0d30ffb45ea05dfb0f1ddd4d9d0a25448e50f
tonypan <tonypan@umich.edu>
2019-01-22 20:42:16 -0500

    updated frame checking program

--- vector_count_frames.py
+++ vector_count_frames.py
@@ -15,15 +15,18 @@ def main():
         start_time = datetime.datetime.now()                            # starting system time
         count = 0                                                       # number of frames
         prev_img = robot.camera.latest_image                            # get the latest frame
-        prev_img_time_stamp = robot.last_image_time_stamp               # get the time stamp of the latest image in ms
+        prev_id = robot.camera.latest_image_id                          # get the image id
+        # prev_img_time_stamp = robot.last_image_time_stamp             # get the time stamp of the latest image in ms
         while (datetime.datetime.now() - start_time).seconds < 10:      # let the program run for 10 seconds
             current_img = robot.camera.latest_image                     # get the latest frame
-            if current_img != prev_img:                             # if the latest frame is different from the previous
+            current_id = robot.camera.latest_image_id                   # get the latest image id
+            if current_id != prev_id:                               # if the latest frame is different from the previous
                 count = count + 1                                       # increment count
-                current_img_time_stamp = robot.last_image_time_stamp  # get the time stamp from the latest frame in ms
-                print((current_img_time_stamp - prev_img_time_stamp) / 100) # print the elapse time in sec
-                prev_img_time_stamp = current_img_time_stamp            # update the previous time stamp
+                # current_img_time_stamp = robot.last_image_time_stamp  # get the time stamp from the latest frame in ms
+                # print((current_img_time_stamp - prev_img_time_stamp) / 100)# print the elapse time in sec
+                # prev_img_time_stamp = current_img_time_stamp          # update the previous time stamp
                 prev_img = current_img                                  # update the previous image
+                prev_id = current_id                                    # update the image id
         print(count / 10)                                               # after ten seconds, print the average fps
 
 
